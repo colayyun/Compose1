1a975dad1e355ad65996c01204fbf85f9d36a830
colayyun <koyu0902@gmail.com>
2024-12-27 02:26:17 +0800

    洪可芸 第三次Commit

--- app/src/main/java/tw/edu/pu/s1110054/compose1/Finger.kt
+++ app/src/main/java/tw/edu/pu/s1110054/compose1/Finger.kt
@@ -1,9 +1,12 @@
 package tw.edu.pu.s1110054.compose1
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
 import android.view.MotionEvent
 import android.view.View
 
@@ -12,10 +15,17 @@ class Finger(context: Context?) : View(context) {
     //var xPos:Float = 200f
     //var yPos:Float = 200f
 
-    var Count:Int = 0
+    var Count:Int = 0;
     var xPos = FloatArray(20)
     var yPos = FloatArray(20)
     var rainbow = IntArray(7)
+
+    lateinit var bitmap: Bitmap
+
+    init {
+        rainbow = resources.getIntArray(R.array.rainbow) // 修正語法
+        bitmap = BitmapFactory.decodeResource(resources, R.drawable.robot)
+    }
 
 
 
@@ -24,11 +34,21 @@ class Finger(context: Context?) : View(context) {
         canvas.drawColor(Color.LTGRAY)
 
         for (i in 0..Count-1) {
-            paint.color = Color.YELLOW
+            paint.color = rainbow[i % 7]
             canvas.drawCircle(xPos[i], yPos[i], 80f, paint)
         }
 
+        paint.color = Color.BLUE
+        paint.textSize = 50f
+        canvas.drawText("多指觸控，圓形呈現彩虹顏色！", 50f,200f, paint)
+
+        var SrcRect: Rect = Rect(0, 0, bitmap.width, bitmap.height) //裁切(顯示全部)
+        var DestRect:Rect = Rect(200, 300,
+            bitmap.width/4+200, bitmap.height/4+300) //原圖較大，縮成1/4顯示
+        canvas.drawBitmap(bitmap, SrcRect, DestRect, paint)
+
     }
+
 
     override fun onTouchEvent(event: MotionEvent): Boolean{
         //xPos = event.getX()
