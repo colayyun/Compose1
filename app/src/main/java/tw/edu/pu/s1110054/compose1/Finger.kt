e82facb012ed4eed972a10564a7c833b557146db
colayyun <koyu0902@gmail.com>
2024-12-27 02:12:03 +0800

    洪可芸 第二次Commit

---
+++ app/src/main/java/tw/edu/pu/s1110054/compose1/Finger.kt
@@ -0,0 +1,47 @@
+package tw.edu.pu.s1110054.compose1
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.MotionEvent
+import android.view.View
+
+class Finger(context: Context?) : View(context) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    //var xPos:Float = 200f
+    //var yPos:Float = 200f
+
+    var Count:Int = 0
+    var xPos = FloatArray(20)
+    var yPos = FloatArray(20)
+    var rainbow = IntArray(7)
+
+
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawColor(Color.LTGRAY)
+
+        for (i in 0..Count-1) {
+            paint.color = Color.YELLOW
+            canvas.drawCircle(xPos[i], yPos[i], 80f, paint)
+        }
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean{
+        //xPos = event.getX()
+        //yPos = event.getY()
+        Count = event.getPointerCount()
+        for (i in 0..Count-1) {
+            xPos[i] = event.getX(i)
+            yPos[i] = event.getY(i)
+        }
+
+        invalidate()
+        return true
+    }
+
+
+}
--- app/src/main/java/tw/edu/pu/s1110054/compose1/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1110054/compose1/MainActivity.kt
@@ -16,17 +16,8 @@ import tw.edu.pu.s1110054.compose1.ui.theme.Compose1Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Compose1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        //setContentView(R.layout.activity_main)
+        setContentView(Finger(this))
     }
 }
 
